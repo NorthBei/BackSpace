198af6a8cf7642fdc0df5912c212dfb9c2b6925c
NorthBei <seekingfor520@gmail.com>
2016-08-14 21:53:15 +0800

    BackSpace v1.0
    backspace相容大部分網頁
    目前發現 Google Map/Keep 不相容

---
+++ contentscript.js
@@ -0,0 +1,20 @@
+document.addEventListener("keydown",function(event){
+	
+	if(event.key == "Backspace"){
+		var focusObj = document.activeElement;
+	
+		if( (focusObj instanceof HTMLInputElement) && (focusObj.type ="text") ){
+			return;
+		}
+		
+		var cursor = window.getComputedStyle(focusObj, null).getPropertyValue("cursor");
+		
+		if( (focusObj instanceof HTMLTextAreaElement) || (cursor == "text") || (focusObj.isContentEditable) ){
+			return;
+		}
+		
+		event.preventDefault();
+		event.stopPropagation();
+		window.history.back();
+	}
+},true);
