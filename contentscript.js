dff04e0f42c7019fa0a20a0420591eb42e185a5c
NorthBei <seekingfor520@gmail.com>
2016-09-08 21:23:28 +0800

    更新至1.0.3
    bug report(fixed):
    fixed that when user onfocus on an input element and type is "search",
    if user can click backspace button , browser will jump to previous page.

--- contentscript.js
+++ contentscript.js
@@ -1,9 +1,9 @@
 document.addEventListener("keydown",function(event){
-	
+	console.log(event.key);
 	if(event.key == "Backspace"){
 		var focusObj = document.activeElement;
-		
-		if( (focusObj instanceof HTMLInputElement) && (focusObj.type == "text" || focusObj.type == "password" ) ){
+				
+		if( (focusObj instanceof HTMLInputElement) && (focusObj.type == "text" || focusObj.type == "password" || focusObj.type == "search") ){
 			return;
 		}
 		
